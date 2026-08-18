43e381941216bbe65ecbd05df87581adeeaade21
andilibrianto <andilibrianto69@gmail.com>
2026-08-19 00:39:14 +0700

    first commit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ranel-cell-cache-v.0.1.3.9';
+const CACHE_NAME = 'ranel-cell-cache-v.0.1.4.0';
 const urlsToCache = [
     './',
     './index.html',
@@ -10,7 +10,6 @@ const urlsToCache = [
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
 
-// Saat Service Worker diinstall, daftarkan file ke cache
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -20,7 +19,6 @@ self.addEventListener('install', event => {
     self.skipWaiting();
 });
 
-// Saa Service Worker diaktifkan, hapus cache versi lama
 self.addEventListener('activate', event => {
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
@@ -28,7 +26,7 @@ self.addEventListener('activate', event => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName); // Hapus cache lama
+                        return caches.delete(cacheName);
                     }
                 })
             );
@@ -37,18 +35,15 @@ self.addEventListener('activate', event => {
     self.clients.claim();
 });
 
-// Strategi Cache: Cache First, fallback ke Network
 self.addEventListener('fetch', event => {
     if (event.request.method !== 'GET') return;
 
-    // Jangan cache API peta (Nominatim) atau API Vercel Backend
     if (event.request.url.includes('nominatim.openstreetmap.org') || event.request.url.includes('vercel.app')) {
         return;
     }
 
     event.respondWith(
         caches.match(event.request).then(response => {
-            // Jika ada di cache, ambil dari cache. Jika tidak, fetch dari internet.
             return response || fetch(event.request).catch(() => caches.match('./index.html'));
         })
     );
