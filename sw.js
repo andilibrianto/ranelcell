c49b6bff6d3aaed31a45e8c5fd6df88721fbf983
andilibrianto <andilibrianto69@gmail.com>
2026-08-27 19:20:44 +0700

    first commit

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'ranel-cell-cache-v.0.2.3.5';
+const CACHE_NAME = 'ranel-cell-cache-v.0.2.3.6';
 const urlsToCache = [
     './',
-    './index.html?v=5.8',
-    './manifest.json?v=v5.8',
+    './index.html',
+    './manifest.json',
     'https://cdn.tailwindcss.com',
     'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
